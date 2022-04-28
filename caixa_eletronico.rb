e6c5be9e3e281e71f0f135bcfb7b5dda23c3cc8b
Augusto Silva <augusto.da.silva@live.com>
2022-04-28 09:10:37 -0300

    Add Método - Validar Cliente

--- caixa_eletronico.rb
+++ caixa_eletronico.rb
@@ -1,5 +1,31 @@
 #Lista de Cliente
-clientes = []
+@clientes = []
+
+
+def validar_cliente
+    
+    print "Digite o CPF vinculado a conta: "
+    @cpf = gets.chomp.to_s
+    print "Digite sua senha: "
+    @senha = gets.chomp.to_s
+    puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
+    puts "\n"
+
+
+    arr = @clientes.select{|i| i[:cpf] == @cpf}
+    @hash = Hash[*arr]
+    
+    if @hash[:cpf] == @cpf && @hash[:senha] == @senha
+        puts "Usuário Autenticado com Sucesso !"
+        puts "Seu saldo é de R$ #{@hash[:saldo]}"
+        puts "\n"
+    else 
+        puts "CPF e / ou Senha estão incorretos. Por favor, tente novamente."
+    end
+
+end
+
+
 
 #Menu de Opções
 loop do
@@ -22,13 +48,13 @@ loop do
     case opcao
     when 1
         # Verificar se existe alguma conta e lista todas
-        puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
+        puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\n"                                                        
         puts "\n"
         
-        if clientes.any? == false
+        if @clientes.any? == false
             puts "Nenhuma Conta Cadastrada!"
         else
-            puts clientes
+            puts @clientes
         end
         
         puts "\n"  
@@ -43,158 +69,86 @@ loop do
         print "Digite a senha: "
         conta_corrente[:senha] = gets.chomp
         conta_corrente[:saldo] = 0
-        clientes.push(conta_corrente)
+        @clientes.push(conta_corrente)
         puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
         puts "\n"
         puts "Conta criada com sucesso !"
         puts "\n"
         
-  
     when 3
-        # Consulta Saldo do cliente com base no CPF
-        print "Digite o CPF vinculado a conta: "
-        cpf = gets.chomp.to_s
-        print "Digite sua senha: "
-        senha = gets.chomp.to_s
-        puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
-        puts "\n"
-        arr = clientes.select{|i| i[:cpf] == cpf}
-        hash = Hash[*arr]
-        if hash[:cpf] == cpf && hash[:senha] == senha
-            puts "Usuário Autenticado com Sucesso !"
-            puts "Seu saldo é de R$ #{hash[:saldo]}"
-        else 
-            puts "CPF e / ou Senha estão incorretos. Por favor, tente novamente."
-        end
-        puts "\n"
+        puts validar_cliente
   
     when 4
-        # Realiza Deposito
-        print "Digite o CPF vinculado a conta: "
-        cpf = gets.chomp.to_s
-        print "Digite sua senha: "
-        senha = gets.chomp.to_s
+        puts validar_cliente
+
+        # Realiza Deposito    
+        print "Digite o valor do deposito: "
+        deposito = gets.chomp.to_i
         
-        puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
+        @hash[:saldo] =  @hash[:saldo] + deposito
+        @clientes.delete_if { |h| h[:cpf] == @cpf }
+        @clientes.push(@hash)
+        
         puts "\n"
-        
-        arr = clientes.select{|i| i[:cpf] == cpf}
-        hash = Hash[*arr]
-        
-        if hash[:cpf] == cpf && hash[:senha] == senha
-            puts "Usuário Autenticado com Sucesso !"
-            puts "Seu saldo é de R$ #{hash[:saldo]}"
-            print "Digite o valor do deposito: "
-            deposito = gets.chomp.to_i
-            hash[:saldo] =  hash[:saldo] + deposito
-            clientes.delete_if { |h| h[:cpf] == cpf }
-            clientes.push(hash)
-            puts "Deposito Realizado com sucesso!" 
-            puts "Seu saldo é de R$ #{hash[:saldo]}"
-        else 
-            puts "CPF e / ou Senha estão incorretos. Por favor, tente novamente."
-        end
-        puts "\n"
+        puts "Deposito Realizado com sucesso!" 
+        puts "Seu saldo é de R$ #{@hash[:saldo]}" 
     
     when 5
-        # Realiza Saque         
-        print "Digite o CPF vinculado a conta: "
-        cpf = gets.chomp.to_s
+        puts validar_cliente
+
+        #Realiza Saque
+        print "Digite o valor do saque: "
+        saque = gets.chomp.to_i
         
-        print "Digite sua senha: "
-        senha = gets.chomp.to_s
-        
-        puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
         puts "\n"
-        
-        arr = clientes.select{|i| i[:cpf] == cpf}
-        hash = Hash[*arr]
-
-        if hash[:cpf] == cpf && hash[:senha] == senha
-            puts "Usuário Autenticado com Sucesso !"
-            puts "Seu saldo é de R$ #{hash[:saldo]}"
-            print "Digite o valor do saque: "
-            saque = gets.chomp.to_i
-            
-            if saque > hash[:saldo]
-                puts "Valor solcitado excede saldo em conta."
-            else
-                hash[:saldo] =  hash[:saldo] - saque
-                clientes.delete_if { |h| h[:cpf] == cpf }
-                clientes.push(hash)
-                puts "Saque Realizado com sucesso!" 
-                puts "Seu saldo é de R$ #{hash[:saldo]}"
-            end
-
-        else 
-            puts "CPF e / ou Senha estão incorretos. Por favor, tente novamente."
-        end 
+        if saque > @hash[:saldo]
+            puts "Valor solcitado excede saldo em conta."
+        else
+            @hash[:saldo] =  @hash[:saldo] - saque
+            @clientes.delete_if { |h| h[:cpf] == @cpf }
+            @clientes.push(@hash)
+            puts "Saque Realizado com sucesso!" 
+            puts "Seu saldo é de R$ #{@hash[:saldo]}"
+        end
         puts "\n"
            
     when 6
-        print "Digite o CPF vinculado a conta: "
-        cpf = gets.chomp.to_s
+        puts validar_cliente
 
-        print "Digite sua senha: "
-        senha = gets.chomp.to_s
+        #Realiza Tranferência 
+        print "Digite o CPF vinculado a conta que deseja transferir: "
+        cpf_transferencia = gets.chomp.to_s
 
-        puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
-        puts "\n"
-
-        arr = clientes.select{|i| i[:cpf] == cpf}
-        hash = Hash[*arr]
-
-        if hash[:cpf] == cpf && hash[:senha] == senha
-            puts "Usuário Autenticado com Sucesso !"
-            print "Digite o CPF vinculado a conta que deseja transferir: "
-            cpf_transferencia = gets.chomp.to_s
-
-            if arr_transferencia = clientes.select{|i| i[:cpf] == cpf_transferencia}
-                hash_transferencia = Hash[*arr_transferencia]
-                print "Digite o valor da transferencia: "
-                valor_transferencia = gets.chomp.to_i
-                
-                if valor_transferencia > hash[:saldo]
-                    puts "Valor informado excede saldo em conta."
-                else
-                    hash[:saldo] =  hash[:saldo] - valor_transferencia
-                    clientes.delete_if { |h| h[:cpf] == cpf }
-                    clientes.push(hash)
-                    hash_transferencia[:saldo] = hash_transferencia[:saldo] + valor_transferencia
-                    clientes.delete_if { |h| h[:cpf] == cpf_transferencia }
-                    clientes.push(hash_transferencia)
-                    puts "Tranferencia Realizada com sucesso!" 
-                    puts "Seu saldo é de R$ #{hash[:saldo]}"
-                end
-            else 
-                puts "CPF e / ou Senha estão incorretos. Por favor, tente novamente."
+        if arr_transferencia = @clientes.select{|i| i[:cpf] == cpf_transferencia}
+            hash_transferencia = Hash[*arr_transferencia]
+            print "Digite o valor da transferencia: "
+            valor_transferencia = gets.chomp.to_i
+            
+            if valor_transferencia > @hash[:saldo]
+                puts "Valor informado excede saldo em conta."
+            else
+                @hash[:saldo] =  @hash[:saldo] - valor_transferencia
+                @clientes.delete_if { |h| h[:cpf] == @cpf }
+                @clientes.push(@hash)
+                hash_transferencia[:saldo] = hash_transferencia[:saldo] + valor_transferencia
+                @clientes.delete_if { |h| h[:cpf] == cpf_transferencia }
+                @clientes.push(hash_transferencia)
+                puts "Tranferencia Realizada com sucesso!" 
+                puts "Seu saldo é de R$ #{@hash[:saldo]}"
             end
         else
             puts "CPF de destino invalido. Por favor, tente novamente."   
         end
+
         puts "\n"
 
     when 7 
+        puts validar_cliente
+
         #Exclui Conta
-        print "Digite o CPF vinculado a conta: "
-        cpf = gets.chomp.to_s
-
-        print "Digite sua senha: "
-        senha = gets.chomp.to_s
-
-        puts " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - \n"                                                        
-        puts "\n"
+        @clientes.delete_if { |h| h[:cpf] == @cpf }
         
-        arr = clientes.select{|i| i[:cpf] == cpf}
-        hash = Hash[*arr]
-
-        if hash[:cpf] == cpf && hash[:senha] == senha
-            clientes.delete_if { |h| h[:cpf] == cpf }
-            puts "Conta encerrada com sucesso!"
-        else 
-            puts "CPF e / ou Senha estão incorretos. Por favor, tente novamente."
-        end
-       
+        puts "Conta encerrada com sucesso!"
         puts "\n"
                 
     when 8
